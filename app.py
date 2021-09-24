d73473a27804b1b4b7357ddeed61d3517988a077
Muice <scm1400@gmail.com>
2021-09-24 14:00:51 +0900

    #7 모달창에 상세정보 크롤링 데이터 전달

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify, render_template
+import re
+
+from flask import Flask, jsonify, render_template, request
 import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 import xml.etree.ElementTree as et
@@ -19,8 +22,29 @@ def test():
     print(res.json())
     return jsonify(res.json())
 
+@app.route('/api/laws/details', methods=['POST'])
+def saving():
+    url_receive = request.form['url_give']
+    # print(url_receive)
 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (HTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
 
+    soup = BeautifulSoup(data.text, 'html.parser')
+    title = soup.select_one('body > div > div.contentWrap > div.subContents > h3').text
+    proposer= soup.select_one('body > div > div.contentWrap > div.subContents > div > div.contIn > div.tableCol01 > table > tbody > tr > td:nth-child(3)').text
+    content = soup.select_one('#summaryContentDiv').text
+    date = soup.select_one('body > div > div.contentWrap > div.subContents > div > div.contIn > div.tableCol01 > table > tbody > tr > td:nth-child(2)').text
+
+    print(title)
+
+    return jsonify({'content': content, 'title':title, 'date':date, 'proposer':proposer})
+
+def clean_text(text):
+    content = text.get_text()
+    cleaned_text = re.sub('[a-zA-Z]', '', content)
+    return cleaned_text
 
 if __name__ == '__main__':
     app.run()
