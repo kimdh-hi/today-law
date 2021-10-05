ab969075ee15b09d78c07d5e914dcbeea55956a6
kimdaehuyn <zbeld123@gmail.com>
2021-10-05 15:37:42 +0900

    #51 카테고리별 법안목록 조회 api 구현 (미완성), html 틀 구성

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import search, app, crawl, rank, like, bookmark
+import search, app, crawl, rank, like, bookmark, category
 
 app = Flask(__name__)
 
@@ -8,6 +8,8 @@ app.register_blueprint(crawl.bp) # 법안 상세페이지 크롤링 API
 app.register_blueprint(rank.bp) # 순위
 app.register_blueprint(like.bp) # 좋아요
 app.register_blueprint(bookmark.bp) # 즐겨찾기
+
+app.register_blueprint(category.bp)
 
 @app.route('/')
 def index():
