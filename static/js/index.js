9bf8fa54d08b280c81ee932d128669a81161bf0f
kimdh-hi <zbeld123@gmail.com>
2021-10-14 10:25:29 +0900

    배포 에러 수정

--- static/js/index.js
+++ static/js/index.js
@@ -145,7 +145,7 @@ $(document).ready(function () {
 
 function mypage() {
     if (is_authenticated === true) {
-        location.href = {EB_URL} + '/mypage'
+        location.href = base_url + '/mypage'
     } else {
         show_login_modal()
     }
