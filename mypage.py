fcdbcf7afd00d2ef9378b3f971ea3d5cad606fec
kimdh-hi <zbeld123@gmail.com>
2021-10-18 13:08:11 +0900

    배포

--- mypage.py
+++ mypage.py
@@ -6,8 +6,8 @@ from pymongo import MongoClient
 MONGO_URL = os.environ['MONGO_URL']
 MONGO_USERNAME = os.environ['MONGO_USERNAME']
 MONGO_PASSWORD = os.environ['MONGO_PASSWORD']
-client = MongoClient(MONGO_URL, 27017)
-# client = MongoClient(MONGO_URL, 27017, username=MONGO_USERNAME, password=MONGO_PASSWORD)
+# client = MongoClient(MONGO_URL, 27017)
+client = MongoClient(MONGO_URL, 27017, username=MONGO_USERNAME, password=MONGO_PASSWORD)
 TOKEN_KEY = os.environ['TOKEN_KEY']
 JWT_SECRET = os.environ['JWT_SECRET']
 
